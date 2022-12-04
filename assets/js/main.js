2b63cc7a0fde7c41c3ada57517358d66c8c7c4d4
Carter Abdallah <112413751+worldconnectionswebsite@users.noreply.github.com>
2022-12-04 16:04:42 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -230,6 +230,33 @@
 
   });
 
+  window.addEventListener('load', () => {
+    let portfolioContainer = select('.portfolio-container2');
+    if (portfolioContainer) {
+      let portfolioIsotope = new Isotope(portfolioContainer, {
+        itemSelector: '.portfolio-item'
+      });
+
+      let portfolioFilters = select('#portfolio-flters li', true);
+
+      on('click', '#portfolio-flters li', function(e) {
+        e.preventDefault();
+        portfolioFilters.forEach(function(el) {
+          el.classList.remove('filter-active');
+        });
+        this.classList.add('filter-active');
+        console.log(this.classList)
+        portfolioIsotope.arrange({
+          filter: this.getAttribute('data-filter')
+        });
+        portfolioIsotope.on('arrangeComplete', function() {
+          AOS.refresh()
+        });
+      }, true);
+    }
+
+  });  
+
   /**
    * Initiate portfolio lightbox 
    */
